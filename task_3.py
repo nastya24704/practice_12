572ac0d791daeddf24b06266357a9525e759e5fb
nastya24704 <nastazeravina@gmail.com>
2025-05-11 01:15:30 +0700

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,1 +1,10 @@
+text = "1234 344 12"
+def simbol(text):
+    count = 0
+    simbole = []
+    for i in text:
+        if i not in simbole:
+            simbole.append(i)
+    return len(simbole)
 
+print(simbol(text))

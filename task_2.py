03ac5cc56fe1f9b55f9fb6497098b141d166e215
nastya24704 <nastazeravina@gmail.com>
2025-05-11 01:04:10 +0700

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,11 @@
-
+def simbol(text):
+    max_count = 1
+    count = 1
+    for i in range(1, len(text)):
+        if text[i]==text[i-1]:
+            count += 1
+            if count > max_count:
+                max_count = count
+        else:
+            count = 1
+    return max_count

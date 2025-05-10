90c8dfec2852c1d2f927043515cc785c92a375a1
nastya24704 <nastazeravina@gmail.com>
2025-05-11 00:55:12 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,1 +1,11 @@
-
+def spaces(text):
+    max_count = 0
+    count = 0
+    for ch in text:
+        if ch == " ":
+            count += 1
+            if count > max_count:
+                max_count = count
+        else:
+            count = 0
+    return max_count

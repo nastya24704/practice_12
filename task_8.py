f3360b2e1b44c4edf02a2c6618aedda4fc399fe4
nastya24704 <nastazeravina@gmail.com>
2025-05-11 23:40:13 +0700

    Update task_8.py

--- task_8.py
+++ task_8.py
@@ -1,1 +1,10 @@
-
+sentence = "настя привет пока нет да зачем почему"
+def new_sentence(sentence):
+  words = sentence.split()
+  n = len(words)
+  for i in range(n):
+      for j in range(0, n - i - 1):
+          if len(words[j]) > len(words[j + 1]):
+              words[j], words[j + 1] = words[j + 1], words[j]
+  return " ".join(words)
+print(new_sentence(sentence))

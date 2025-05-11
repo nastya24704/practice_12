b2af9ab1e6edc91ccd06e0fcfd7a11a105a8d380
nastya24704 <nastazeravina@gmail.com>
2025-05-11 17:30:29 +0700

    Update task_6.py

--- task_6.py
+++ task_6.py
@@ -1,1 +1,7 @@
-
+text = 'nastya dfgh fgh fg'
+def opposite_definition(text):
+    word = text.split(' ')
+    sentence = word[::-1]
+    opposite_sentence = ''.join(sentence)
+    return sentence
+print(opposite_definition(text))

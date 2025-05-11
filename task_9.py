523fdbe441c6de381899d2e8ab1c99a878c8a253
nastya24704 <nastazeravina@gmail.com>
2025-05-12 00:29:59 +0700

    Update task_9.py

--- task_9.py
+++ task_9.py
@@ -1,1 +1,11 @@
+text = 'tre tre re ert ret ter rew'
 
+def find_duplicate_words(text):
+    words = text.split()
+    for i in range(len(words)):
+        for j in range(i + 1, len(words)):
+            if words[i] == words[j]:
+                return words[i]
+    return None  
+
+print(find_duplicate_words(text))

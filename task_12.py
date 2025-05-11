8027e5f1af5a5e86c8f1732dc0c5e763b6f9bc64
nastya24704 <nastazeravina@gmail.com>
2025-05-12 06:47:58 +0700

    Update task_12.py

--- task_12.py
+++ task_12.py
@@ -1,1 +1,19 @@
+import keyword
 
+def is_valid_python_identifier(name):
+    if not name:
+        return False
+    if keyword.iskeyword(name):
+        return False
+    first_char = name[0]
+    if not (first_char.isalpha() or first_char == '_'):
+        return False
+    for char in name[1:]:
+        if not (char.isalnum() or char == '_'):
+            return False
+    
+    return True
+
+test_names = ["nastya_name", "2types", "_privet", "class", "import", "MyType"]
+for name in test_names:
+    print(f"'{name}': {'Да' if is_valid_python_identifier(name) else 'Нет'}")

915e457e59e1b70471ff0e07020445ee0cd99543
nastya24704 <nastazeravina@gmail.com>
2025-05-12 01:51:22 +0700

    Update task_10.py

--- task_10.py
+++ task_10.py
@@ -1,1 +1,25 @@
+text = 'tre tre re ert ret ter rew'
 
+def new_words(text):
+    words = text.split()
+    first_word = words[0]
+    result = []
+
+    for word in words[1:]:
+        if word == first_word:
+            continue
+        has_duplicates = False
+        for i in range(len(word)):
+            for j in range(i + 1, len(word)):
+                if word[i] == word[j]:
+                    has_duplicates = True
+                    break
+            if has_duplicates:
+                break
+
+        if not has_duplicates:
+            result.append(word)
+
+    print(*result if result else "Таких слов нет")
+
+print(new_words(text))
